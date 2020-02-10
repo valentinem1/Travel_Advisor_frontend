cc54ebd664cc8fb48ccc670ac7198122b50b23fd
Valentine <valentinemaillard1@gmail.com>
2020-02-10 11:46:10 -0500

    routes and components

--- travel_advisor_frontend/src/App.js
+++ travel_advisor_frontend/src/App.js
@@ -1,13 +1,33 @@
 import React, { Component } from 'react'
 import './App.css';
+import { Route, Switch } from 'react-router'
+import SignUp from 'SignUp'
+import Home from './HomeComponents/HomeContainer'
+import ProfileContainer from './ProfileComponents/ProfileContainer'
+import ShowContainer from './DestinationComponents/ShowContainer'
 
 class App extends Component {
+
+  renderProfileCont = (routerProps) => {
+    console.log(routerProps)
+  }
+
+  renderDestinationCont = (routerProps) => {
+    console.log(routerProps)
+  }
 
   render() {
 
     return (
       <div>
-        HELLO
+       <Switch>
+        <Header />
+        <DropDown />
+        <Route exact path='/' component={ Home } />
+        <Route path='/profile/:id' render={ this.renderProfileCont } />
+        <Route path='/signup' component={ SignUp } />
+        <Route path='/:destination' render={ this.renderDestinationCont } />
+       </Switch>
       </div>
     )
   }
