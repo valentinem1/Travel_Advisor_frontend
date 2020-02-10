f7e6af9cefc0c2a09d5671f950f0565e24d449c2
Valentine <valentinemaillard1@gmail.com>
2020-02-10 17:41:17 -0500

    fetchcreate user

--- travel_advisor_frontend/src/SignUp.jsx
+++ travel_advisor_frontend/src/SignUp.jsx
@@ -1,6 +1,7 @@
 import React, {Component} from 'react';
 
 class SignUp extends Component {
+
   state = {
     username: "",
     bio:"",
@@ -15,13 +16,38 @@ class SignUp extends Component {
     })
   }
 
+  // creating new user and giving it a new token. 
+  // did fetch here for re-routing to home "/" due to App.js not knowing about routerProps.
   handleSubmit=(evt)=>{
     evt.preventDefault()
+    fetch('http://localhost:4000/users', {
+      method: "POST",
+      headers:{
+        "Content-Type": "application/json",
+        "Accept": "application/json"
+      },
+      body: JSON.stringify(this.state)
+    })
+    .then(r => r.json())
+    .then(newUser => {
+      console.log(newUser)
+      if (!newUser.error) {
+        localStorage.setItem("token", newUser.token)
+        // invoking createNewUser coming from App.js as callback functionwith the newUser instance as an argument to set the state in App.js.
+        this.props.createNewUser(newUser)
+        
+        // coming from App.js to re-route to home "/" after creating user.
+        this.props.routerProps.history.push("/")
+        
+      }
+    })
   }
 
   render() {
-    return (
 
+    // console.log(this.props)
+
+    return (
 
         <form onSubmit={this.handleSubmit}>
           <label htmlFor="username"><b>Username</b></label>
@@ -54,7 +80,7 @@ class SignUp extends Component {
                 <input
                   type="password"
                   placeholder="Enter Password"
-                  name="psw"
+                  name="password"
                   value={this.state.password}
                   onChange={this.handleAllChange}
                   />
