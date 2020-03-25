fa99edd6750839edff02731ac7e51eb528f7dc59
Valentine <valentinemaillard1@gmail.com>
2020-03-25 18:31:08 -0400

    working on review card

--- travel_advisor_frontend/src/DestinationComponents/CommentContainer.jsx
+++ travel_advisor_frontend/src/DestinationComponents/CommentContainer.jsx
@@ -1,22 +1,22 @@
 import React, { Component } from 'react';
 import CommentCard from './CommentCard'
 import CommentForm from './CommentForm'
-import { Header, Container } from 'semantic-ui-react'
+import { Header, Container, Segment } from 'semantic-ui-react'
 
 class CommentContainer extends Component {
 
   render() {
 
     let commentCard = this.props.destination.reviews ?  this.props.destination.reviews.map(review => <CommentCard key={review.id} review={review} user={this.props.user} deleteReview={this.props.deleteReview} routerProps={this.props.routerProps} /> ) : null
-    
-    // console.log(this.props)
 
     return (
-      <Container className="comment-container">
-        <Header className="review-container-header">Reviews for this destination:</Header>
-          {commentCard}
-        <CommentForm createComment={this.props.createComment} />
-      </Container>
+      <Segment className="comment-segment">
+        <Container className="comment-container">
+          <Header className="review-container-header">Reviews for this destination:</Header>
+            {commentCard}
+          <CommentForm createComment={this.props.createComment} />
+        </Container>
+      </Segment>
     );
   }
 
